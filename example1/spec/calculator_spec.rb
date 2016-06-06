d056a30584f397e514e88b10b6abe6cfe550f1ed
rodriggochaves <rodriggo.chaves@gmail.com>
2016-06-06 19:44:46 -0300

    exemplo de TDD

--- example1/spec/calculator_spec.rb
+++ example1/spec/calculator_spec.rb
@@ -1,17 +1,17 @@
 require 'spec_helper'
 
 RSpec.describe Calculator do
-  before(:example) do
-    @calculator = Calculator.new
-  end
+  # before(:example) do
+  #   @calculator = Calculator.new
+  # end
 
   describe "#add" do
     it 'returns the sum of its arguments ' do
-      expect(@calculator.add(1,2)).to eq(3)
+      expect(Calculator.new.add(1,2)).to eq(3)
     end
 
     it 'can calculate with negative param' do
-      expect(@calculator.add(1, -2)).to eq(-1)
+      expect(Calculator.new.add(1, -2)).to eq(-1)
     end
   end
 end
